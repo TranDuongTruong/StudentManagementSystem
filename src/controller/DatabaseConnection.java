eb196641c44c29a427b0155b11c6a7e2079774c3
vvm1004 <96302642+vvm1004@users.noreply.github.com>
2023-11-21 23:07:44 +0700

    Database Online

--- src/controller/DatabaseConnection.java
+++ src/controller/DatabaseConnection.java
@@ -11,11 +11,16 @@ import model.Student;
 import model.ClassesManager;
 
 public class DatabaseConnection {
-	 private static Connection con = null;
+	 private static Connection con = null;	 
 	 //FOR Local
-	    private static final String URL = "jdbc:mysql://sql12.freesqldatabase.com:3306/sql12663967";   
-	    private static final String USER = "sql12663967";
-	    private static final String PASSWORD = "ASq18HYwTP";
+	    private static final String URL = "jdbc:mysql://localhost:3306/spmdatabase11";   
+	    private static final String USER = "root";
+	    private static final String PASSWORD = "";
+	    
+	 //FOR Online
+//	    private static final String URL = "jdbc:mysql://sql12.freesqldatabase.com:3306/sql12664007";   
+//	    private static final String USER = "sql12664007";
+//	    private static final String PASSWORD = "NJ3l7Ikjd1";
 
 	 
 	 //FOR OTHER
