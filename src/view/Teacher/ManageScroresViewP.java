ad939c7bc4ba4d04ad25523ea6ca2ee8876b10cc
vvm1004 <96302642+vvm1004@users.noreply.github.com>
2023-12-07 01:21:06 +0700

    Done manage score interface

--- src/view/Teacher/ManageScroresViewP.java
+++ src/view/Teacher/ManageScroresViewP.java
@@ -42,7 +42,6 @@ import model.Classroom;
 import model.Student;
 
 public class ManageScroresViewP extends JPanel {
-	  private JTextField textField;
 	  private JTable table;
 	  private static final long serialVersionUID = 1L;
 	   Classroom classroom;
@@ -55,27 +54,6 @@ public class ManageScroresViewP extends JPanel {
 		this.classCode = classCode;
 		setLayout(null);
         setBounds(162, 0, 835, 640);
-
-        JLabel label_Student_ID = new JLabel("Student ID");
-        label_Student_ID.setFont(new Font("Tahoma", Font.PLAIN, 19));
-        label_Student_ID.setBounds(28, 39, 135, 42);
-        add(label_Student_ID);
-
-        textField = new JTextField();
-        textField.setFont(new Font("Tahoma", Font.PLAIN, 19));
-        textField.setColumns(10);
-        textField.setBounds(185, 43, 272, 35);
-        add(textField);
-
-        JButton btnTim = new JButton("Search");
-        btnTim.setFont(new Font("Tahoma", Font.PLAIN, 18));
-        btnTim.setBounds(512, 41, 117, 41);
-        add(btnTim);
-
-        JButton btnHuyTim = new JButton("Undo");
-        btnHuyTim.setFont(new Font("Tahoma", Font.PLAIN, 18));
-        btnHuyTim.setBounds(649, 41, 117, 41);
-        add(btnHuyTim);
 
         JLabel lblListOfScores = new JLabel("List of scores");
         lblListOfScores.setFont(new Font("Tahoma", Font.PLAIN, 19));
