9d460c392166d59990398de9727f00634d417679
TUDG <tranduongtruong1623@gmail.com>
2023-12-13 19:43:59 +0700

    Change style

--- src/controller/DatabaseConnection.java
+++ src/controller/DatabaseConnection.java
@@ -16,7 +16,7 @@ public class DatabaseConnection {
 	 //FOR Local
 	    private static final String URL = "jdbc:mysql://localhost:3306/spmdatabase11";   
 	    private static final String USER = "root";
-	    private static final String PASSWORD = "123456";
+	    private static final String PASSWORD = "";
 	    
 	 //FOR Online
 //	    private static final String URL = "jdbc:mysql://sql12.freesqldatabase.com:3306/sql12663967";   
